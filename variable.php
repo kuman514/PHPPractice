98fea9d2f4fa20fa49a65b564ec8ab1bac799e24
kumankoishi.py <7iggif@naver.com>
2021-07-24 17:01:29 +0900

    Practicing variable

---
+++ variable.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <body>
+    <h1>Variable</h1>
+    <?php
+      $name = "koishi";
+      echo "Lorem Ipsum $name Tiempo ";
+      echo "Lorem Ipsum ".$name." Tiempo by ".$name;
+      $number = 100;
+      echo $number + 33;
+    ?>
+  </body>
+</html>
