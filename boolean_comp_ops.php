b42c4f032d10ff435efb3aed47354ed8c12668b8
kumankoishi.py <7iggif@naver.com>
2021-07-25 00:02:29 +0900

    Practicing boolean and comparison operations

---
+++ boolean_comp_ops.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Boolean Data Type and Comparison Operators</h1>
+    <h2>1==1</h2>
+    <?php
+      var_dump(1 == 1);
+    ?>
+    <h2>1>1</h2>
+    <?php
+      var_dump(1 > 1);
+    ?>
+    <h2>1>=1</h2>
+    <?php
+      var_dump(1 >= 1);
+    ?>
+  </body>
+</html>
