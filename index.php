b280785c0e67dfd5a371f6ac30eea97c20cf2fd6
kumankoishi.py <7iggif@naver.com>
2021-07-30 00:01:11 +0900

    Added delete button on app index

--- index.php
+++ index.php
@@ -40,6 +40,7 @@
   <head>
     <meta charset="utf-8">
     <title><?php printWindowTitle(); ?></title>
+    <link rel="stylesheet" ref="operation_button.css">
   </head>
   <body>
     <h1><a href="index.php">kuman514 memo app</a></h1>
@@ -52,8 +53,11 @@
     <?php
       if (isset($_GET['id']) && file_exists('data\\'.$_GET['id'])) { ?>
         <a href="update.php?id=<?=$_GET['id']?>">UPDATE</a>
-        <?php
-      }
+        <form action="delete_process.php" method="POST">
+          <input type="hidden" name="id" value="<?=$_GET['id']?>">
+          <input type="submit" class="delete-button" value="delete">
+        </form>
+      <?php }
     ?>
     <h2>
       <?php
